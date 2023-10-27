a6bd1e41b42901fa5bc7dd3e8826d1aaa547b8f4
FernandoReyesUTS <jfernandoreyes@uts.edu.co>
2023-10-27 17:30:55 -0500

    reserva

--- Backend/Models/Reserva.js
+++ Backend/Models/Reserva.js
@@ -3,10 +3,15 @@ const Usuario = require('./Usuario');
 
 const Reserva = mongoose.Schema({
     fechaReserva: { type: String, unique: true, require: true },
-    horaReserva: { type: Number, unique: true, require: true },
-    numeroPersonas: { type: String, require: true },
-    //cliente: { type: Usuario, require: true },
-    estado: [{ type: String, require: true }]
+    horaReserva: { type: String, unique: true, require: true },
+    numeroPersonas: { type: Number, require: true },
+    cliente: {type: mongoose.Schema.Types.ObjectId,ref: 'Usuario'},
+    estado: [
+        {
+            type: String,
+            enum: ['Confirmada', 'Cancelada'] // Define los estados permitidos aquí
+        }
+    ]
 }, {
     collection: 'Reserva',
     versionKey: false
